f023a6988909a8204b756f0ebd504d67d927c9fb
Catherine Leung <cathyatseneca@gmail.com>
2015-02-20 17:10:35 -0500

    binary search tree

--- bst.cpp
+++ bst.cpp
@@ -5,10 +5,16 @@ struct Node{
 	int data_;
 	Node* left_;
 	Node* right_;
+	Node(int data,Node* left=nullptr,Node*right=nullptr){
+		data_=data;
+		left_=left;
+		right_=right;
+	}
 };
 
 class BST{
 	Node* root_;
+	Node* searchR(int data,Node* subtreeroot);
 public:
 	BST();
 	void insert(int data);
@@ -21,7 +27,33 @@ BST::BST(){
 	root_=nullptr;
 }
 void BST::insert(int data){
-
+	if(root_){
+		Node* curr=root_;
+		bool found=false;
+		while(!found){
+			if(data < curr->data_){
+				if(curr->left_){
+					curr=curr->left_;
+				}
+				else{
+					curr->left_=new Node(data);
+					found=true;
+				}
+			}
+			else{
+				if(curr->right_){
+					curr=curr->right_;
+				}
+				else{
+					curr->right_=new Node(data);
+					found=true;
+				}
+			}
+		}
+	}
+	else{
+		root_=new Node(data);
+	}
 }
 
 Node* BST::search(int data){
@@ -36,8 +68,24 @@ Node* BST::search(int data){
 	}
 	return curr;
 }
-
-
+Node* BST::searchR(int data){
+	return searchR(data,root_);
+}
+Node* BST::searchR(int data,Node* subtreeroot){
+	Node* rc=nullptr;
+	if(subtreeroot){
+		if(data == subtreeroot->data_){
+			rc=subtreeroot;
+		}
+		else if(data < subtreeroot->data_){
+			rc=searchR(data,subtreeroot->left_);
+		}
+		else{
+			rc=searchR(data,subtreeroot->right_);
+		}
+	}
+	return rc;
+}
 
 
 
